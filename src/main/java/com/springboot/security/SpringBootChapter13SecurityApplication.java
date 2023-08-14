b4de0757292a5ebb6607385db7a8dacc0124b876
Eclipse_ <nightsky5070@gmail.com>
2023-08-14 16:55:20 +0900

    file

---
+++ src/main/java/com/springboot/security/SpringBootChapter13SecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootChapter13SecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootChapter13SecurityApplication.class, args);
+    }
+
+}
